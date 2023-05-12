37c53ffc62b713e0c62c3c71dfd3fb1c73c2e2b7
Khoi Nguyen <62054711+nguyenkhoi2412@users.noreply.github.com>
2023-05-12 20:00:41 +0700

    .

--- _routes/file.routes.js
+++ _routes/file.routes.js
@@ -1,22 +1,25 @@
 import express from "express";
 import file from "../controllers/file.controller.js";
+import verifyTokenJWT from "../middleware/authJwt.js";
 const routerFile = express.Router();
 
 //#region ARTICLES
 // POST: api/file/upload/
-routerFile.route("/upload/:type?/:identifyfolder?/").post((req, res) => {
-  const { type } = req.params;
+routerFile
+  .route("/upload/:type?/:identifyfolder?/")
+  .post(verifyTokenJWT, (req, res) => {
+    const { type } = req.params;
 
-  switch (type) {
-    case "multiple":
-      file.UPLOAD_FILES(req, res);
-      break;
+    switch (type) {
+      case "multiple":
+        file.UPLOAD_FILES(req, res);
+        break;
 
-    default:
-      file.UPLOAD_FILE_SINGLE(req, res);
-      break;
-  }
-});
+      default:
+        file.UPLOAD_FILE_SINGLE(req, res);
+        break;
+    }
+  });
 //#endregion
 
 export default routerFile;
