ccea0e214d6d30a307bae309f5e4092b014a0fdd
Khoi Nguyen <62054711+nguyenkhoi2412@users.noreply.github.com>
2023-12-03 18:07:55 +0700

    .

--- utils/cache/redis.instance.js
+++ utils/cache/redis.instance.js
@@ -1,29 +1,36 @@
-// import redis from "redis";
-// let cache = null;
+import redis from "redis";
+let cache = null;
 
-// const redisInstance = {
-//   start: async () => {
-//     cache = await redis
-//       .createClient(process.env.CACHE_PORT || 6379)
-//       .on("error", (err) => console.log("Redis Client Error", err))
-//       .on("connect", (err) => console.log("Redis Client Started"))
-//       .connect();
-//   },
-//   instance: () => {
-//     return cache;
-//   },
-//   get: async (key) => {
-//     const value = await cache.get(key);
-//     return JSON.parse(value);
-//   },
-//   set: async (key, value) => {
-//     // expire default is 60 * 5 => 300min
-//     await cache.set(key, JSON.stringify(value), process.env.CACHE_DURATION || 600);
-//   },
-//   del: async (key) => {
-//     cache.del(key);
-//   },
-//   clearAll: async () => {},
-// };
+const redisInstance = {
+  start: async () => {
+    cache = await redis
+      .createClient({
+        host: process.env.REDIS_HOST || "localhost",
+        port: process.env.CACHE_PORT || 6379,
+      })
+      .on("error", (err) => console.log("Redis Client Error", err))
+      .on("connect", (err) => console.log("Redis Client Started"))
+      .connect();
+  },
+  instance: () => {
+    return cache;
+  },
+  get: async (key) => {
+    const value = await cache.get(key);
+    return JSON.parse(value);
+  },
+  set: async (key, value) => {
+    // expire default is 60 * 5 => 300min
+    await cache.set(
+      key,
+      JSON.stringify(value),
+      process.env.CACHE_DURATION || 600
+    );
+  },
+  del: async (key) => {
+    cache.del(key);
+  },
+  clearAll: async () => {},
+};
 
-// export default redisInstance;
+export default redisInstance;
